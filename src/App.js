0f3ed83816fde89e592000f54e567f404c5f050b
Gaston Ndanyuzwe <ndanyuzwe@hotmail.com>
2019-11-05 20:51:49 +0100

    ref hook & effect hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -20,19 +20,28 @@ const Display = styled.div`
 
 function App() {
     const [value, setValue] = useState('');
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        const input = inputRef.current;
+        input.addEventListener('input', handleChange);
+        return function clean() {
+            input.removeEventListener('input', handleChange)
+        }
+    }, [value]);
 
     function handleChange(e) {
-        e.persist()
-        const withStars = e.eventPhase;
+        const withStars = e.target.value;
         const withoutStars = withStars.split('').filter(c => c !== '*').join('');
         setValue(withoutStars)
     }
+    
   return (
     <Wrapper>
         <Display>
             {value}
         </Display>
-      <input onChange={event => handleChange(event)} />
+      <input ref={inputRef} />
       {/* <input onChange={event => handleChange(event)} value={value}/> */}
     </Wrapper>
   );
