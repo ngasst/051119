27464da3228ad1614eb8e5d9830fdee1afebb529
Gaston Ndanyuzwe <ndanyuzwe@hotmail.com>
2019-11-05 20:37:32 +0100

    demo useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    background-color: salmon;
+    width: 500px;
+    height: 500px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-flow: column nowrap;
+`;
+
+const Display = styled.div`
+    color: darkred;
+    font-weight: 700;
+    font-size: 14px;
+`
 
 function App() {
+    const [value, setValue] = useState('');
+
+    function handleChange(e) {
+        e.persist()
+        const withStars = e.eventPhase;
+        const withoutStars = withStars.split('').filter(c => c !== '*').join('');
+        setValue(withoutStars)
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+        <Display>
+            {value}
+        </Display>
+      <input onChange={event => handleChange(event)} />
+      {/* <input onChange={event => handleChange(event)} value={value}/> */}
+    </Wrapper>
   );
 }
 
